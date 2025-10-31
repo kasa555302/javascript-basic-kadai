f5d1f8bdbf7a9094a18273aabd43ccea78ebb190
kasa555302 <bibibi3jp@yahoo.co.jp>
2025-11-01 05:59:03 +0900

    課題005を追加

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+// 変数の宣言・値の代入
+let youWeight = 68;
+let youHeight = 1.7;
+
+// コンソールへの出力
+console.log(youWeight);
+console.log(youHeight);
+
+// 定数の宣言・値の代入
+const yourBmi = youWeight / ( youHeight * youHeight );
+
+// コンソールへの出力
+console.log(yourBmi);
